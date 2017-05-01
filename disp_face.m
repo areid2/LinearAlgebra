6a8b832a4c99758968923d28eedcf7ebfaeabed5
ptakahashi <ptakahashi@umassd.edu>
2017-05-01 19:29:03 -0400

    disp face update

--- disp_face.m
+++ disp_face.m
@@ -1,6 +1,4 @@
 function [] = disp_face(v)
-    Y = zeros(32*1,32*1); 
-    Y(0*32+1:(0+1)*32,0*32+1:(0+1)*32) = reshape(v,[32,32]); 
-    imagesc(Y);colormap(gray);
+    imagesc(reshape(v,[32,32]));
+    colormap(gray);
 end
- %seeing if this works just as well
