99a0c04bd110ba2814aa52331cff2f86be0b63e0
ptakahashi <ptakahashi@umassd.edu>
2017-05-01 22:49:01 -0400

    lightning fast optimization
    added 'econ' argument to svd

--- thinHOSVD.m
+++ thinHOSVD.m
@@ -1,6 +1,6 @@
 function [S, U1, U2, U3] = thinHOSVD(A)
-    [U1,~,~] = svd(unfold(A,1));
-    [U2,~,~] = svd(unfold(A,2));
-    [U3,~,~] = svd(unfold(A,3));
+    [U1,~,~] = svd(unfold(A,1),'econ');
+    [U2,~,~] = svd(unfold(A,2),'econ');
+    [U3,~,~] = svd(unfold(A,3),'econ');
     S = tmul(tmul(tmul(A,U1',1),U2',2),U3',3);
 end
