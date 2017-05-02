0a127a4d785e8b91f5e761b03f4f57a2bd047885
ptakahashi <ptakahashi@umassd.edu>
2017-05-01 22:15:05 -0400

    added link to tmul

--- tmul.m
+++ tmul.m
@@ -1,3 +1,4 @@
+%https://www.mathworks.com/matlabcentral/fileexchange/24268-n-mode-tensor-matrix-product
 function B = tmul(A,M,n)
     K = shiftdim(A,n-1);
     B = shiftdim(reshape(M*K(:,:),[size(M,1),size(K,2),size(K,3)]),4-n);
