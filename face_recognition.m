cd2dd9e943803d18f5bea7be3dc55fa29e558d99
ptakahashi <ptakahashi@umassd.edu>
2017-05-01 20:19:48 -0400

    replaced numbers with variables

--- face_recognition.m
+++ face_recognition.m
@@ -3,11 +3,17 @@ clear; close; clc;
 %preprocessing
 load('yale32.mat');
 
+%dimensions
+l = 32;
+w = 32;
+e = 11;
+p = 15;
+
 %image vector length, pictures per person, number of people
-a = zeros(1024,11,15);
-for i=1:15
-    for j=1:11
-        a(:,j,i)=fea((i-1)*11+j,:);
+a = zeros(l*w,e,p);
+for i=1:p
+    for j=1:e
+        a(:,j,i)=fea((i-1)*e+j,:);
     end
 end
 
@@ -21,12 +27,12 @@ z=a(:,7,8);
 c = tmul(tmul(s,f,1),g,2);
 soln = -1;
 
-for e = 1:11
-    t = permute(c(:,e,:),[1,3,2]);
+for ee = 1:e
+    t = permute(c(:,ee,:),[1,3,2]);
     ae = (t'*t)\t'*z;
-    for p=1:15
-        if norm((ae-h(p,:)'),2) < 0.001
-            soln = p;
+    for pp=1:p
+        if norm((ae-h(pp,:)'),2) < 0.001
+            soln = pp;
         end
     end
 end
